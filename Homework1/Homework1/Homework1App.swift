26b7ad9091159326d7d98166d5bf4ca3e7db7eee
moni <monadj548@gmail.com>
2026-09-14 16:46:45 +0700

    app moi

---
+++ Homework1/Homework1/Homework1App.swift
@@ -0,0 +1,32 @@
+//
+//  Homework1App.swift
+//  Homework1
+//
+//  Created by MAY 01 on 14/9/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Homework1App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Homework1/Homework1Tests/Homework1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Homework1Tests.swift
+//  Homework1Tests
+//
+//  Created by MAY 01 on 14/9/26.
+//
+
+import Testing
+@testable import Homework1
+
+struct Homework1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
